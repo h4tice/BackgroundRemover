b97b7837fb623440ac7834635bc802be37427713
h4tice <haticekrc772@gmail.com>
2023-04-13 17:47:17 +0300

    Background Remover

---
+++ backgroundRemover/main.py
@@ -0,0 +1,10 @@
+from rembg import remove
+
+input_path = 'input.png'
+output_path = 'output.png'
+
+with open(input_path, 'rb') as i:
+    with open(output_path, 'wb') as o:
+        input_file = i.read()
+        output_file = remove(input)
+        o.write(output)
